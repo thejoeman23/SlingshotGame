c569192a7efd0f15719eb10cba1ec2d61c2ebbfd
thejoeman23 <epicjonathan24@gmail.com>
2024-10-05 10:32:43 -0400

    game demo build

--- Assets/Script/Cursor.cs
+++ Assets/Script/Cursor.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
-using UnityEngine.WSA;
 
 public class Cursor : MonoBehaviour
 {
